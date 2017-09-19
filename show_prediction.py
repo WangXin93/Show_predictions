340009ea8c3ffdff5e6f19eafda7535677fdd067
WangXin93 <wangxin19930411@163.com>
2017-09-19 19:02:50 +0800

    Add comments

--- show_prediction.py
+++ show_prediction.py
@@ -1,3 +1,4 @@
+# Import dependencies
 import tensorflow as tf
 from tensorflow.contrib import slim
 import os
@@ -8,13 +9,23 @@ from datasets import imagenet
 from nets import vgg
 from preprocessing import vgg_preprocessing
 
+# Set parameter
 FILE_DIR = '/home/wangx/project/show_single_pred/images'
 IMAGE_SIZE = vgg.vgg_16.default_image_size
 NUM_EPOCHS = 10
 CHECKPOINTS_DIR = '/home/wangx/project/show_single_pred/checkpoints'
 names = imagenet.create_readable_names_for_imagenet_labels()
+NUM_IMAGES = 3    # how many image you want to show
 
+# Helper function to get list of filenames, labels
 def get_files(file_dir):
+    '''
+    Args:
+        file_dir: directory include images
+    Returns:
+        filenames: list of path of images
+        labels: list of labels corresponding to images
+    '''
     filenames = []
     labels = []
 
@@ -27,6 +38,7 @@ def get_files(file_dir):
     print('For example: ',filenames[0])
     return filenames, labels
 
+# Map function to dataset
 def _parse_function(filename, label):
     image_string = tf.read_file(filename)
     image_decoded = tf.image.decode_jpeg(image_string, channels=3)
@@ -39,9 +51,12 @@ def _parse_function(filename, label):
     raw_img = image_resized
     return raw_img, processed_img, label
 
+# Get filename list and label list
 filenames, labels = get_files(FILE_DIR)
 
 with tf.Graph().as_default():
+
+    # Use tf.contrib.data.Dataset to load batch samples
     dataset = tf.contrib.data.Dataset.from_tensor_slices((filenames, labels))
     dataset = dataset.map(_parse_function)
     #dataset = dataset.shuffle(buffer_size=10000)
@@ -51,26 +66,36 @@ with tf.Graph().as_default():
 
     next_raw_img, next_processed_img, next_label = iterator.get_next()
     
+    # Use model to infer probabilities
     with slim.arg_scope(vgg.vgg_arg_scope()):
         logit, _ = vgg.vgg_16(next_processed_img, num_classes=1000, is_training=False)
 
     probabilities = tf.nn.softmax(logit)
     
+    # Set function to restore weights
     init_fn = slim.assign_from_checkpoint_fn(
         os.path.join(CHECKPOINTS_DIR, 'vgg_16.ckpt'),
         slim.get_model_variables('vgg_16'))
         
     with tf.train.MonitoredTrainingSession() as sess:
+
+        # Load weights
         init_fn(sess)
+
+        # Set loop numbers here
         i = 0
-        while not sess.should_stop() and i<10:
+
+        while not sess.should_stop() and i<NUM_IMAGES:
             np_raw_img, np_processed_img, np_probabilities = sess.run([next_raw_img,
                                                                        next_processed_img,
                                                                        probabilities])
             np_probabilities = np_probabilities[0, 0:]
+            
+            # Get an ordered list of index by descending probabilities
             sorted_idx = [i[0] for i in sorted(
                 enumerate(-np_probabilities), key=lambda x:x[1])]
             
+            # Print predictions and save it to draw
             pred_class = []
             pred_prob = []
             for t in range(5):
@@ -78,9 +103,10 @@ with tf.Graph().as_default():
                 print("Probability %0.2f => [%s]" % (np_probabilities[index], names[index+1]))
                 pred_class.append(names[index+1])
                 pred_prob.append(np_probabilities[index])
-                
+             
+            # Show raw image and preprocessed image and top-5 predictions   
             plt.rcdefaults()
-            fig = plt.figure()
+            fig = plt.figure(figsize=(16,9))
             ax1 = plt.subplot2grid((2,2),(0,0))
             ax2 = plt.subplot2grid((2,2),(0,1))
             ax3 = plt.subplot2grid((2,2),(1,0), colspan=2)
@@ -93,7 +119,7 @@ with tf.Graph().as_default():
             ax2.axis('off')
             
             y_pos = np.arange(len(pred_class))
-            ax3.barh(y_pos, pred_prob)
+            ax3.barh(y_pos, pred_prob, color='green')
             ax3.set_yticks(y_pos)
             ax3.set_yticklabels(pred_class)
             ax3.invert_yaxis()
